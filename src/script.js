11b0b4683f08555aa7ed31cdf7ac77602d2d1a5c
justritzy <alvarovilelanova@gmail.com>
2024-06-20 18:20:46 +0200

    Added logos and download buttons. Made tech cards appear when scrolled down and gave them format.

--- src/script.js
+++ src/script.js
@@ -14,3 +14,20 @@ document.addEventListener('DOMContentLoaded', function () {
         observer.observe(card);
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.card-t');
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    cards.forEach(card => {
+        observer.observe(card);
+    });
+});
